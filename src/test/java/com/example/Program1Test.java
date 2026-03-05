56c76a7153a8c2c86f72f6c0c6c1ecb742ccb65d
ajaysharan2006 <ajaysharan.r2023@vitstudent.ac.in>
2026-03-05 15:26:41 +0530

    pipe

---
+++ src/test/java/com/example/Program1Test.java
@@ -0,0 +1,9 @@
+package com.example;
+import org.junit.*;
+import static org.junit.Assert.*;
+public class Program1Test {
+    public void test(){
+        Program1 o = new Program1();
+        assertNotNull(o)
+    }
+}
